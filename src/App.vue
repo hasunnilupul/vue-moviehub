44d39134edabb203df632bec6171dd5bc9cae040
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-03-01 18:44:18 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+        <RouterLink to="/" class="text-base">Home</RouterLink>
+        <RouterLink to="/about" class="text-base">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    Home
+  </main>
+</template>
